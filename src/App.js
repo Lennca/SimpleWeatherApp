b40dd91877bd21d69904259e0c96ef173d1281d4
Caesar Lennartsson <cl223mf@student.lnu.se>
2020-10-14 23:41:53 +0200

    Create react app. Background image set

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="background_container d-flex justify-content-center">
+      <h1>Weather App</h1>
+    </div>
+  );
+}
+
+export default App;
